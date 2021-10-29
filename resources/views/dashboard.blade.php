8bf0d446b643b25f55e4bd3011a02c32278656f1
marw593 <marwa.mamm79@gmail.com>
2021-10-29 03:42:05 +0200

    simple style

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,12 +1,19 @@
 @extends('layouts.master')
 
+@section('title')
+Facebook
+
+@endsection
+
 @section('content')
 
-@include('includes.message-block')
+
 
 
 <section class="row new-post">
         <div class="col-md-6 col-md-offset-3">
+            
+            @include('includes.message-block')
             <header><h3>What do you have to say?</h3></header>
             <form action="{{ route('post.create') }}" >
                 <div class="form-group">
